4596a1f7ed05f886037d312bf6ec413b37238623
Anderson <117619499+idrissdjassa@users.noreply.github.com>
2025-04-27 20:37:13 +0200

    Update NewsItem.js

--- fake_news_react-master/src/components/NewsItem.js
+++ fake_news_react-master/src/components/NewsItem.js
@@ -7,9 +7,7 @@ class NewsItem extends React.Component {
       <article className="article-list-stories" id={id}>
         <h1 className="article-titles">{title}</h1>
         <p>
-          Spicy jalapeno bacon ipsum dolor amet ham hock tri-tip tongue biltong bresaola. Drumstick
-          shankle meatloaf kevin rump filet mignon. Meatball rump fatback, pork spare ribs pancetta
-          short ribs cupim pastrami frankfurter strip steak.
+          
         </p>
         <div>
           <a href={url}>Article Home</a> | <i className="far fa-thumbs-up" /> {likes} |{" "}
